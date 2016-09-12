d121066983eaf19cb8bea90941537eca022cd119
Christian Page <page.ntnu@gmail.com>
2016-09-12 10:14:40 +0200

    Added a functional vignette and change to ASCII

--- R/pakage-name.R
+++ R/pakage-name.R
@@ -1,25 +1,7 @@
 #' DMRScan: An R-package for identification of Differentially Metylated Regions 
-#' 
-#' 
-#' 
-#' @author Christian Page, \email{christian.page@medisin.uio.no}
+#' @author Christian Page, \email{page.ntnu@gmail.com}
 #' @references \url{http://Some_link_to_BMC-bioInfomatics.com}
 #' @keywords DMR
-#' 
-#' 
-#' 
-#' 
-#' 
-#' 
-#' 
-#' 
-#' 
-#' 
-#'  
-#' 
-#' 
-#' 
-#' 
 #' @docType package
 #' @name DMRScan
 NULL
